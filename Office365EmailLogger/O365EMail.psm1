d069c0b87d2063984aee997272e8b18771ccfa76
Joe Richards <richardswjoe@gmail.com>
2017-02-02 14:29:43 +0000

    Update O365EMail.psm1

--- Office365EmailLogger/O365EMail.psm1
+++ Office365EmailLogger/O365EMail.psm1
@@ -6,7 +6,7 @@
    - The StoreCreds checks to see if the folder and files exist for authenticating to Office365 and if it doesnt exist, then stores the credentials to C:\o365\.
    - The SendO354Mail function retrives the files which were created with StoreCreds function and sends the Email to Office365 email address.
    - Nothing has to be changed for this to work except under Send-MailMessage, the -To and the -From.
-.Author
+.Notes
    Joe RIchards
    Date: 02/Feb/2017
 .LINK
