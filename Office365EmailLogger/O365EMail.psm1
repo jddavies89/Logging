2f90ed3014a6aa2bc3a007e6c8c33d3e2c42be20
Joe Richards <richardswjoe@gmail.com>
2017-02-02 14:29:58 +0000

    Update O365EMail.psm1

--- Office365EmailLogger/O365EMail.psm1
+++ Office365EmailLogger/O365EMail.psm1
@@ -7,7 +7,7 @@
    - The SendO354Mail function retrives the files which were created with StoreCreds function and sends the Email to Office365 email address.
    - Nothing has to be changed for this to work except under Send-MailMessage, the -To and the -From.
 .Notes
-   Joe RIchards
+   Author: Joe RIchards
    Date: 02/Feb/2017
 .LINK
   https://github.com/joer89/Logging
