10b58324bd00f6e06e8dbddc1487834737caf516
Joe Richards <joe.richards@theregisschool.co.uk>
2017-02-02 12:35:44 +0000

    Office365 Email Logger Module.

--- Office365EmailLogger/O365EMail.psm1
+++ Office365EmailLogger/O365EMail.psm1
@@ -1,4 +1,16 @@
-﻿#Checks to see if the file exists if not it prompts for the password and creates a file with the encrypted password.
+﻿<#
+.Synopsis
+   This Module sends Office365 emails for logging.
+.DESCRIPTION
+   - There is two functions for this module, StoreCred and send0365Mail.
+   - The StoreCreds checks to see if the folder and files exist for authenticating to Office365 and if it doesnt exist, then stores the credentials to C:\o365\.
+   - The SendO354Mail function retrives the files which were created with StoreCreds function and sends the Email to Office365 email address.
+   - Nothing has to be changed for this to work except under Send-MailMessage, the -To and the -From.
+.LINK
+  https://github.com/joer89/Logging
+#>
+
+#Checks to see if the file exists if not it prompts for the password and creates a file with the encrypted password.
 function StoreCreds{
 
         if((-Not (Test-Path -LiteralPath "C:\O365\O365User.txt")) -or (-Not (Test-Path -LiteralPath "C:\O365\O365Pass.txt"))){   
@@ -25,11 +37,9 @@ function send0365Mail{
        if((Test-Path -LiteralPath "C:\O365\O365User.txt") -and (Test-Path -LiteralPath "C:\O365\O365Pass.txt")){ 
 
             #Gets the Office365 credentials.
-            $User = Get-Content "C:\O365\O365User.txt"
-              
+            $User = Get-Content "C:\O365\O365User.txt"              
             #Gets the Office365 credentials.
             $Pass = Get-Content "C:\O365\O365Pass.txt" | ConvertTo-SecureString
-
             #Adds the username and password together and puts it in the Credentials variable.
             $credential = New-Object System.Management.Automation.Pscredential -Argumentlist $User, $Pass
                     
@@ -43,8 +53,8 @@ function send0365Mail{
                 #Writes out to the screen.
                 Write-Error "Message error."
             }#end try.
-    }
-    else{
+        }
+        else{
             #Creates the credentials for office365.
             StoreCreds
         }#End if.
