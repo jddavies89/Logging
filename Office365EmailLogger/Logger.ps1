5569fe680b175971de61df9882ced3f6c02cd4a3
Joe Richards <joe.richards@theregisschool.co.uk>
2017-02-02 12:23:13 +0000

    Example of using the module.

---
+++ Office365EmailLogger/Logger.ps1
@@ -0,0 +1,24 @@
+﻿param(
+    #Retrives the execution directory and Stores the content of $body.
+    $path = (split-path $script:MyInvocation.MyCommand.Path -parent),
+    $body
+)#end paramter.
+
+#imports the module.
+function ImpModule{
+    #Imports the O365Logger module.
+    Import-Module  $path\O365EMail.psm1
+}#end function.
+
+
+#Imports the office365 logger module from ImpModule function.
+ImpModule
+
+#Creates the credentials for office365.
+StoreCreds
+
+#Stores the body content of the emails.
+$body = "This script started at $(Get-Date -format "dd-MMM-yyyy HH:mm")"
+
+#Sends the Office365 email with the body content.
+send0365Mail $body
